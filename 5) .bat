b08d451d50ffb38b8ad6a043c9648c12fb298799
Xerexcide <max_subbotin@bk.ru>
2024-10-01 16:03:56 +0300

    upd fin

---
+++ 5) .bat
@@ -0,0 +1,6 @@
+@Echo off
+set /P A=enter A
+set /P B=enter B
+set /A C=%A%+%B%
+Echo %C%
+Pause
--- 6) .bat
+++ 6) .bat
@@ -1,1 +1,21 @@
+@ECHO OFF
+IF %1 LEQ 0 GOTO Exit
+IF %1 GEQ 34 GOTO Exit
 
+SET /A n=%1
+SET /A f=%1
+:BegLoop
+
+IF %n% == 1 GOTO Loop
+SET /A n = n-1
+SET /A f = f*n
+GOTO BegLoop
+
+:Loop
+ECHO %1!=%f%
+Exit /b
+
+:Exit
+ECHO not correnct parameter
+Exit /b
+pause
